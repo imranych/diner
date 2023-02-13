50038789c56f341833e2d1c517bafa217a410a59
rusmos <106593813+imranych@users.noreply.github.com>
2023-02-13 13:53:21 +0300

    2

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const paymentForm = document.getElementById('payment-form')
 const customerNameInput = document.getElementById('customer-name')
 const closeModalBtn = document.getElementById('close-modal-btn')
 const renderName = document.getElementById('render-name')
+const orderRemove = document.getElementsByClassName("order__remove")
 
 closeModalBtn.addEventListener('click', function() {
     modalSection.style.display = 'none'
@@ -25,6 +26,15 @@ paymentForm.addEventListener('submit', function(e) {
     orderBtn.style.display = 'none'
     thanksSection.style.display = 'flex'
     renderName.innerText = customerName
+    for (let i = 0; i<orderRemove.length; i++) {
+        orderRemove[i].classList.add('hidden')
+    }
+    for (let item of orderArray) {
+        if (item.quantity === 0) {
+            console.log(`${item.name} was not ordered`)
+        }
+    }
+    //console.log(orderArray)
 })
 
 let itemsContent = ``
@@ -40,7 +50,7 @@ function collectItemsHTML() {
               <div class="item__details">
                 <div class="item__name" id="item__name">${item.name}</div>
                 <div class="item__ingredients" id="item__ingredients">${item.ingredients.join(', ')}</div>
-                <div class="item__price" id="item__price">$${item.price}</div>
+                <div class="item__price" id="item__price"><span class="dollar">$</span>${item.price}</div>
               </div>
               <div class="item__add">
                 <button class="add-btn" id="${item.id}" data-${item.name.toLowerCase()}="${item.id}">+</button>
@@ -56,8 +66,8 @@ function collectOrderListHTML() {
             orderListContent += `
                 <div class="list__item">
                     <span class="order__name" id="${item.name.toLowerCase()}">${item.name}</span>
-                    <span class="order__remove" data-${item.name.toLowerCase()}-del="${item.id}-del">remove</span>
-                    <span class="order__price" id="${item.name.toLowerCase()}-order">$0</span>
+                    <span class="order__remove" id="order__remove" data-${item.name.toLowerCase()}-del="${item.id}-del">remove</span>
+                    <span class="order__price" id="${item.name.toLowerCase()}-order"><span class="dollar">$</span>0</span>
                 </div>
             `
     })
@@ -91,7 +101,7 @@ function handlePizzaClick() {
         orderArray[0].quantity++
         orderArray[0].price += 14
         pizzaOrder.innerHTML = `
-            <span class="order__price" id="pizza-order">${orderArray[0].quantity} x $${orderArray[0].price}</span>
+            <span class="order__price" id="pizza-order">${orderArray[0].quantity} x <span class="dollar">$</span>${orderArray[0].price}</span>
             `
         collectTotal()
 }
@@ -100,7 +110,7 @@ function handleHamburgerClick() {
         orderArray[1].quantity++
         orderArray[1].price += 12
         hamburgerOrder.innerHTML = `
-            <span class="order__price" id="hamburger-order">${orderArray[1].quantity} x $${orderArray[1].price}</span>
+            <span class="order__price" id="hamburger-order">${orderArray[1].quantity} x <span class="dollar">$</span>${orderArray[1].price}</span>
             `    
         collectTotal()
 }
@@ -109,7 +119,7 @@ function handleBeerClick() {
         orderArray[2].quantity++
         orderArray[2].price += 12
         beerOrder.innerHTML = `
-            <span class="order__price" id="beer-order">${orderArray[2].quantity} x $${orderArray[2].price}</span>
+            <span class="order__price" id="beer-order">${orderArray[2].quantity} x <span class="dollar">$</span>${orderArray[2].price}</span>
             `    
         collectTotal()
 }
@@ -127,7 +137,7 @@ function collectTotal() {
     let beer = Number(beerTotalString.slice(beerTotalString.lastIndexOf('$') + 1))
 
     let total = pizza + hamburger + beer
-    totalPrice.innerHTML = `$${total}`    
+    totalPrice.innerHTML = `<span class="dollar">$</span>${total}`    
     totalTotal = total
 }
 
@@ -146,14 +156,14 @@ function handlePizzaDelClick() {
         orderArray[0].quantity--
         orderArray[0].price -= 14
         pizzaOrder.innerHTML = `
-            <span class="order__price" id="pizza-order">${orderArray[0].quantity} x $${orderArray[0].price}</span>
+            <span class="order__price" id="pizza-order">${orderArray[0].quantity} x <span class="dollar">$</span>${orderArray[0].price}</span>
             `
         collectTotal()
     } else if (orderArray[0].quantity === 1) {
         orderArray[0].price -= 14
         orderArray[0].quantity--
         pizzaOrder.innerHTML = `
-            <span class="order__price" id="pizza-order">$${orderArray[0].price}</span>
+            <span class="order__price" id="pizza-order"><span class="dollar">$</span>${orderArray[0].price}</span>
             `
         collectTotal()
     }
@@ -164,14 +174,14 @@ function handleHamburgerDelClick() {
         orderArray[1].quantity--
         orderArray[1].price -= 12
         hamburgerOrder.innerHTML = `
-            <span class="order__price" id="hamburger-order">${orderArray[1].quantity} x $${orderArray[1].price}</span>
+            <span class="order__price" id="hamburger-order">${orderArray[1].quantity} x <span class="dollar">$</span>${orderArray[1].price}</span>
             `
         collectTotal()
     } else if (orderArray[1].quantity === 1) {
         orderArray[1].price -= 12
         orderArray[1].quantity--
         hamburgerOrder.innerHTML = `
-            <span class="order__price" id="hamburger-order">$${orderArray[1].price}</span>
+            <span class="order__price" id="hamburger-order"><span class="dollar">$</span>${orderArray[1].price}</span>
             `
         collectTotal()
     }
@@ -182,15 +192,14 @@ function handleBeerDelClick() {
         orderArray[2].quantity--
         orderArray[2].price -= 12
         beerOrder.innerHTML = `
-            <span class="order__price" id="beer-order">${orderArray[2].quantity} x $${orderArray[2].price}</span>
+            <span class="order__price" id="beer-order">${orderArray[2].quantity} x <span class="dollar">$</span>${orderArray[2].price}</span>
             `
         collectTotal()
-    } 
-    if (orderArray[2].quantity === 1) {
+    } else if (orderArray[2].quantity === 1) {
         orderArray[2].price -= 12
         orderArray[2].quantity--
         beerOrder.innerHTML = `
-            <span class="order__price" id="beer-order">$${orderArray[2].price}</span>
+            <span class="order__price" id="beer-order"><span class="dollar">$</span>${orderArray[2].price}</span>
             `
         collectTotal()
     }
